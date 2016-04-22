b2dc6ee91ff358d577bc34c679689645c65673a5
Avi Charlop <avicharlop@gmail.com>
2016-04-22 13:21:11 -0400

    Concert seeds working, concert index sorted by soonest

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,5 @@
 class ConcertsController < ApplicationController
+	def index
+		@concerts = Concert.order(date: :asc)
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def generate(number)
+  charset = Array('A'..'Z') + Array('a'..'z')
+  Array.new(number) { charset.sample }.join
+end
+
+def generate_word
+	generate(rand 3..9)
+end
+
+def generate_sentence
+	(rand 10..20).times.collect {generate_word }.join(" ")
+end
+
+def generate_short
+	(rand 1..5).times.collect {generate_word }.join(" ")
+end
+
+def rand_date
+	(1..500).to_a.sample.days.from_now
+end
+
+venues = ["The Fillmore Miami Beach",
+					"Awarehouse",
+					"Olympia Theater",
+					"Bayfron Park Ampitheatre",
+					"Soho Studios"]
+
+20.times do
+	Concert.create(
+		artist: generate_short ,
+		venue: venues.sample , 
+		city: "Miami",
+		price: rand(0.0..500.99) ,
+		date: rand_date ,
+		description: generate_sentence
+		)
+end
